527b5a205f2dbc90ae4cdc8a08b41564c2d318c5
Sserpentine <73420073+Sserpentine@users.noreply.github.com>
2021-06-25 20:06:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const a = document.getElementById("a");
+const b = document.getElementById("b");
+const c = document.getElementById("c");
+const d = document.getElementById("d");
+const e = document.getElementById("e");
+const f = document.getElementById("f");
+const g = document.getElementById("g");
+
+a.onclick=()=>{
+    var sound1=new Audio ("https://res.cloudinary.com/dgg7zx0p8/video/upload/v1624612107/tom-1_mouyue.mp3")
+    sound1.play();
+}
+b.onclick=()=>{
+    var sound2=new Audio ("https://res.cloudinary.com/dgg7zx0p8/video/upload/v1624612107/tom-2_ooryy1.mp3")
+    sound2.play();
+}
+c.onclick=()=>{
+    var sound3=new Audio ("https://res.cloudinary.com/dgg7zx0p8/video/upload/v1624612107/tom-3_ose4zi.mp3")
+    sound3.play();
+}
+d.onclick=()=>{
+    var sound4=new Audio ("https://res.cloudinary.com/dgg7zx0p8/video/upload/v1624612107/tom-4_pqk2oj.mp3")
+    sound4.play();
+}
+e.onclick=()=>{
+    var sound5=new Audio ("https://res.cloudinary.com/dgg7zx0p8/video/upload/v1624612107/snare_kzkkyz.mp3")
+    sound5.play();
+}
+f.onclick=()=>{
+    var sound6=new Audio ("https://res.cloudinary.com/dgg7zx0p8/video/upload/v1624612108/crash_jmxpuv.mp3")
+    sound6.play();
+}
+g.onclick=()=>{
+    var sound6=new Audio ("https://res.cloudinary.com/dgg7zx0p8/video/upload/v1624612107/kick-bass_ovbjvj.mp3")
+    sound6.play();
+}
